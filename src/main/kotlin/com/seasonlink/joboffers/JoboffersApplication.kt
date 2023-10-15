33ba61d806347e09538c7ff16b25e2907e012b0e
Martin Moreira de Jesus <martin.moreiradj@icloud.com>
2023-10-15 19:01:30 +0200

    Initial commit

---
+++ src/main/kotlin/com/seasonlink/joboffers/JoboffersApplication.kt
@@ -0,0 +1,11 @@
+package com.seasonlink.joboffers
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JoboffersApplication
+
+fun main(args: Array<String>) {
+    runApplication<JoboffersApplication>(*args)
+}
---
+++ src/test/kotlin/com/seasonlink/joboffers/JoboffersApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.seasonlink.joboffers
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class JoboffersApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
